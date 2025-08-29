0236326f0f47c7d1d65576a7f4e88160748a458b
AlexLandisBSU <alex.landis@bsu.edu>
2025-08-29 16:02:34 -0400

    Update random_project.c

--- random_project.c
+++ random_project.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
-	printf("Hello World! from me! \n");
+	int guess = -1;
+	int correct;
+	int tries = 0;
+	
+	srand(time(NULL));
+	
+	right = rand() % 100;
+	printf(correct);
 	return(0);
 }
 
