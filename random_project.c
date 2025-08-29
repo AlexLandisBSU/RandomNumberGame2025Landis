d45311d5ecc3c1745f49372cc9f2129c2ae4b8a0
AlexLandisBSU <alex.landis@bsu.edu>
2025-08-29 16:46:37 -0400

    Update random_project.c

--- random_project.c
+++ random_project.c
@@ -10,7 +10,8 @@ int main(){
 	char name[20]; //hold name with 20 character limit
 	printf("Hello, what is your name? ");
 	scanf("%s", name);
-	printf("Hello, %s! Lets play a game.", name, "\n");
+	printf("Hello, %s! ", name);
+	printf("Let's play a game.");
 	
 	srand(time(NULL)); //random seed
 	right = rand() % 100; //random number 1-100 but might only go to 99
